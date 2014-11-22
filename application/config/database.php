c3cb36298b7f701f59d8e84b62d1f4da01d9e7d1
metthal <milkovic.marek@gmail.com>
2014-11-22 01:05:04 +0100

    [Config] Changed CodeIgniter config files for DB, modules, XSS filtering, encryption etc.

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'localhost:/var/run/mysql/mysql.sock';
+$db['default']['username'] = 'xmilko01';
+$db['default']['password'] = 'ejum2uno';
+$db['default']['database'] = 'xmilko01';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
