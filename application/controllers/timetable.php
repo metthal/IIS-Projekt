93a74eb74fd35ebbc2cf539c985ab84a411f3e32
metthal <milkovic.marek@gmail.com>
2014-11-27 03:03:37 +0100

    [Web] * Let the timetable controller calculate the timetable offsets * Added template for doc.html file

--- application/controllers/timetable.php
+++ application/controllers/timetable.php
@@ -30,8 +30,48 @@ class Timetable extends CI_Controller
             return;
 
         $events = $this->room_model->schedules_for($roomID, $date);
+        $timetable = array();
+
+        if ($events != false)
+        {
+            foreach ($events as &$event)
+            {
+                $start_hour = date('G', strtotime($event->datum_konania));
+                $duration = $event->trvanie;
+
+                $cols = count($timetable);
+                for ($i = 0; $i < $cols; $i++)
+                {
+                    for ($j = $start_hour; $j < min($start_hour + $duration, 24); $j++)
+                    {
+                        if ($timetable[$i][$j] != 0)
+                            break;
+                    }
+
+                    // there is free place in timetable, insert our event
+                    if ($j == min($start_hour + $duration, 24))
+                    {
+                        $timetable[$i][$start_hour] = array($duration, $event->nazov);
+                        for ($j = $start_hour + 1; $j < min($start_hour + $duration, 24); $j++)
+                            $timetable[$i][$j] = 1;
+
+                        $j = -1;
+                        break;
+                    }
+                }
+
+                // there is no free place, create new column
+                if ($i == $cols)
+                    $timetable = $timetable + array($i => array_fill(0, 24, 0));
+
+                $timetable[$i][$start_hour] = array($duration, $event->nazov);
+                for ($j = $start_hour + 1; $j < min($start_hour + $duration, 24); $j++)
+                    $timetable[$i][$j] = 1;
+            }
+        }
+
         $data = array(
-            'events' => $events
+            'timetable' => $timetable
         );
 
         $this->load->view('timetable_get_view', $data);
--- application/views/timetable_get_view.php
+++ application/views/timetable_get_view.php
@@ -1,46 +1,9 @@
 <table>
 <?php
 
-$timetable = array(/*0 => array_fill(0, 24, 0)*/);
-
-foreach ($events as &$event)
-{
-    $start_hour = date('G', strtotime($event->datum_konania));
-    $duration = $event->trvanie;
-
-    $cols = count($timetable);
-    for ($i = 0; $i < $cols; $i++)
-    {
-        for ($j = $start_hour; $j < min($start_hour + $duration, 24); $j++)
-        {
-            if ($timetable[$i][$j] != 0)
-                break;
-        }
-
-        // there is free place in timetable, insert our event
-        if ($j == min($start_hour + $duration, 24))
-        {
-            $timetable[$i][$start_hour] = array($duration, $event->nazov);
-            for ($j = $start_hour + 1; $j < min($start_hour + $duration, 24); $j++)
-                $timetable[$i][$j] = 1;
-
-            $j = -1;
-            break;
-        }
-    }
-
-    // there is no free place, create new column
-    if ($i == $cols)
-        $timetable = $timetable + array($i => array_fill(0, 24, 0));
-
-    $timetable[$i][$start_hour] = array($duration, $event->nazov);
-    for ($j = $start_hour + 1; $j < min($start_hour + $duration, 24); $j++)
-        $timetable[$i][$j] = 1;
-}
-
 /*for ($hour = 0; $hour < 24; $hour++)
 {
-    echo '<tr><td>', $hour, ':00 - ', ($hour + 1) % 24, ':00</td>';
+    echo '<tr><td>', $hour, ':00</td>';
     for ($col = 0; $col < count($timetable); $col++)
     {
         if ($timetable[$col][$hour] == 1)
@@ -55,7 +18,7 @@ foreach ($events as &$event)
 
 echo '<tr>';
 for ($hour = 0; $hour < 24; $hour++)
-    echo '<td>', $hour, ':00 - ', ($hour + 1) % 24, ':00</td>';
+    echo '<td>', $hour, ':00</td>';
 echo '</tr>';
 
 for ($row = 0; $row < count($timetable); $row++)
